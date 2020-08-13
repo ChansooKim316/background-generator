1d62a4bba873b659c87e59414bcad7250644cd59
ChansooKim316 <cccssk@naver.com>
2020-08-13 18:33:49 +0900

    added random button

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector('button');
 
 
 function setGradient() {
+	var css = document.querySelector('h3');
+
 	body.style.background = 
 						"linear-gradient(to right, " 
 						+ color1.value 
@@ -15,11 +17,30 @@ function setGradient() {
 
 }
 
+function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
 
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.round(Math.random() * 16)];
+	}
+
+	return color;
+}
+
+function setRandomColors() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+	
+	setGradient();
+}
+
+// the second parameter automatically gets ran with the event. 
+body.addEventListener('onload', setGradient());
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
-	//  ㄴ> second parameter automatically gets ran "with the event". 
-	// no () needed. ("input", setGradient()); --> wrong. won't work
+button.addEventListener('click', setRandomColors);
+
+
 
 
